a555d4162df73b9c93f0bbc84eb9665f711bb91a
tarassavenko <taras10@hotmail.gr>
2026-07-25 16:48:58 +0300

    Retrieved best-matching snippet from multiple docs

--- main.py
+++ main.py
@@ -22,10 +22,22 @@ response=client.responses.create(
 
 #print(response.output_text)
 
+
+doc1="""The Greek Cuisine is considered amongst the best in the world.
+        One of the best dishes of the Greek Cuisine is Mousakas.
+        It was ranked #1 in 2024.
+        """
+
 doc="""In the summer of 2006,Italy won the Mundial of 2006.
        They beat France in the penalties 5-4.
        That Year,Canavaro was the best football player of the world.
        """
+doc2="""One genre of movies is science fiction.
+        Lord of the Rings in considered a science fiction movie but it is also considered the best trilogy in cinema history.
+        The duration of the whokle trilogy is estimated around 12 hours
+        """
+documents=[doc,doc1,doc2]
+embeddings_dict={}  
 
 def get_embedding(text,model):
     text=text.replace("\n"," ")
@@ -42,10 +54,26 @@ def cosine_similarity(a,b):
 
     return product_dot/(norm_a * norm_b)
 
-response1=get_embedding(doc,"text-embedding-3-small")
-response2=get_embedding("Did Italy won Mundial on 2006?","text-embedding-3-small")
+for i,doc in enumerate(documents):
+    snippet=" ".join(doc.split()[:10])
 
-score=cosine_similarity(response1,response2)
-print(score)
-     
+    embeddings_dict[f"doc{i+1}"]={
+        "snippet":snippet,
+        "embedding":get_embedding(doc,"text-embedding-3-small")
+    }
+
+question_embedding=get_embedding("Is Greek Cuisine the best in the world?And if yes which year?","text-embedding-3-small")
+
+similarities={}
+
+for doc_name,doc_data in embeddings_dict.items():
+    similarity=cosine_similarity(question_embedding,doc_data["embedding"])
+    print(doc_name,similarity)
+
+    similarities[doc_name]=similarity
+
+print(similarities)    
+
+best_doc=max(similarities,key=similarities.get)
+print(best_doc,similarities[best_doc],embeddings_dict[best_doc]["snippet"])
 
